5d0f8883d17ff70ec2f3deca79c26d2c8afc808d
AmitRaj2001 <amit.12018323@lpu.in>
2023-08-16 00:12:07 +0530

    modified:   src/App.js 	new file:   src/components/Message.jsx 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React from "react";
+import {
+    Box,
+    Button,
+    Input,
+    Container,
+    VStack,
+    HStack,
+} from "@chakra-ui/react";
 
+import Message from "./components/Message";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <Box bg={"red.100"}>
+            <Container h={"100vh"} bg={"white"}>
+                <VStack h="full" paddingY={"4"}>
+                    <Button colorScheme="blackAlpha" w={"full"}>
+                        Logout
+                    </Button>
+
+                    <VStack h="full" w={"full"}>
+                      <Message  text={"Sample message"}/>
+                      <Message user="me" text={"Sample message"}/>
+                      <Message  text={"Sample message"}/>
+                    </VStack>
+                    <form style={{ width: "100%", borderRadius:"20rem" }}>
+                        <HStack>
+                            <Input w={"full"} placeholder="Type a message...."/>
+                            <Button type="submit" colorScheme={"green"}>
+                                Send
+                            </Button>
+                        </HStack>
+                    </form>
+                </VStack>
+            </Container>
+        </Box>
+    );
 }
 
 export default App;
