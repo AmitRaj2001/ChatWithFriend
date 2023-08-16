84c7bf5abd5907563261db234ad98a27ff3885a8
AmitRaj2001 <amit.12018323@lpu.in>
2023-08-17 00:04:45 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/Message.jsx 	new file:   src/firebase.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState,useRef, useEffect } from "react";
 import {
     Box,
     Button,
@@ -7,33 +7,148 @@ import {
     VStack,
     HStack,
 } from "@chakra-ui/react";
-
+import { app } from "./firebase";
 import Message from "./components/Message";
+import {
+    onAuthStateChanged,
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    
+} from "firebase/auth";
+
+import {
+    getFirestore,
+    addDoc,
+    collection,
+    serverTimestamp,
+    onSnapshot,
+    query,
+    orderBy,
+} from "firebase/firestore";
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const loginHandler = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider);
+};
+
+const logoutHandler = () => {
+    signOut(auth);
+};
 
 function App() {
+    
+    const [user, setUser] = useState(false);
+    const [message, setMessage] = useState("");
+    const [massages, setMassages] = useState([]);
+    const devForScroll = useRef(null);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            setMessage("");
+            await addDoc(collection(db, "Messages"), {
+                text: message,
+                uid: user.uid,
+                uri: user.photoURL,
+                createdAt: serverTimestamp(),
+            });
+            
+            devForScroll.current.scrollIntoView({ behavior: "smooth"});
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    useEffect(() => {
+        const q = query(collection(db,"Messages"), orderBy("createdAt","asc"));
+
+
+        const unsubscribe = onAuthStateChanged(auth, (data) => {
+            setUser(data);
+        });
+
+        const unsubscribeForMessage = onSnapshot(q, (snap) => {
+            setMassages(snap.docs.map((item) => {
+                const id = item.id;
+                return {id, ...item.data() };
+            })
+            );
+        });
+        
+
+        return () => {
+            unsubscribe();
+            unsubscribeForMessage();
+        };
+    },[]);
+
     return (
         <Box bg={"red.100"}>
-            <Container h={"100vh"} bg={"white"}>
-                <VStack h="full" paddingY={"4"}>
-                    <Button colorScheme="blackAlpha" w={"full"}>
-                        Logout
-                    </Button>
+            {user ? (
+                <Container h={"100vh"} bg={"white"}>
+                    <VStack h="full" paddingY={"4"}>
+                        <Button
+                            onClick={logoutHandler}
+                            colorScheme="blackAlpha"
+                            w={"full"}
+                        >
+                            Logout
+                        </Button>
 
-                    <VStack h="full" w={"full"}>
-                      <Message  text={"Sample message"}/>
-                      <Message user="me" text={"Sample message"}/>
-                      <Message  text={"Sample message"}/>
+                        <VStack h="full" w={"full"} overflowY={"auto"}
+                            css={{
+                                "&::-webkit-scrollbar": {
+                                    display: "none",
+                                },
+                            }}
+                        >
+                            {
+                                massages.map(item => (
+                                    <Message 
+                                    key = {item.id}
+                                    user={item.uid===user.uid ? "me" : "other"}
+                                    text= {item.text} 
+                                    uri={item.uri}
+                                    />
+                                ))
+                            }
+                            <dev ref={devForScroll}></dev>
+                        </VStack>
+                        <form
+                            onSubmit={submitHandler}
+                            style={{ width: "100%", borderRadius: "20rem" }}
+                        >
+                            <HStack>
+                                <Input
+                                    
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    placeholder="Type a message...."
+                                />
+                                <Button type="submit" colorScheme={"green"}>
+                                    Send
+                                </Button>
+                            </HStack>
+                        </form>
                     </VStack>
-                    <form style={{ width: "100%", borderRadius:"20rem" }}>
-                        <HStack>
-                            <Input w={"full"} placeholder="Type a message...."/>
-                            <Button type="submit" colorScheme={"green"}>
-                                Send
-                            </Button>
-                        </HStack>
-                    </form>
+                </Container>
+            ) : (
+                <VStack
+                    bg="white"
+                    h="100vh"
+                    justifyContent={"center"}
+                >
+                    <Button colorScheme={"purple"} onClick={loginHandler}>
+                        Login with Google
+                    </Button>
                 </VStack>
-            </Container>
+            )}
         </Box>
     );
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,7 @@ import {Avatar,HStack,Text} from "@chakra-ui/react"
 
 const Message = ({text,uri,user="other"}) => {
   return (
-    <HStack alignSelf={user==="me"?"flex-end":"flex-start"} borderRadius={"3xl"} bg={"gray.100"} paddingX={"4"} paddingY={2}>
+    <HStack alignSelf={user==="me" ? "flex-end" : "flex-start"} borderRadius={"3xl"} bg={"gray.100"} paddingX={user==="me"? "4":"2"} paddingY={2}>
         {
             user==="other" && <Avatar src={uri} size={"sm"} />
         }
